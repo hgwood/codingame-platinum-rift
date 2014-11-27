2584daacd9bd44217d4df2aa08338f22c3617451
Hugo Wood <hx4.5@free.fr>
2014-11-27 01:54:02 +0100

    removed dead code

--- platinum_rift.py
+++ platinum_rift.py
@@ -59,13 +59,6 @@ def place_pods(zones, npods):
     return npods - (i + 1)
 
 
-def continent(zone):
-    if zone <= 49: return "america"
-    if zone in (143, 149, 150): return "japan"
-    if zone in (57, 67, 78, 89, 97, 104, 113): return "antartica"
-    return "eaao"
-def not_antartica(zone):
-    return continent(zone) != "antartica"
 def owner(zone):
     return _zone_states[zone][0]
 def owned_by(zone, player):
